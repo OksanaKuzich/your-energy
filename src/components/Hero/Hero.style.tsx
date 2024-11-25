9211a5a6539cbcf9bd92136affd9af7c8580f7c0
Oksana Kuzich <komoks255@gmail.com>
2024-11-25 21:50:01 +0100

    added style for hero section

--- src/components/Hero/Hero.style.tsx
+++ src/components/Hero/Hero.style.tsx
@@ -3,6 +3,8 @@ import { device } from "../../utiles/devices";
 
 export const HeroWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding-bottom: 32px;
 
   /* @media ${device.tablet} {
     display: flex;
@@ -12,4 +14,24 @@ export const HeroWrapper = styled.div`
   @media ${device.desktop} {
     gap: 50px;
   } */
+`;
+
+export const TagSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const TagList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  max-width: 197px;
+`;
+
+export const TagItem = styled.li`
+  border: 1px solid #242424;
+  padding: 4px 16px;
+  border-radius: 30px;
+  font-size: ${(props) => props.theme.fontSizes.fs16};
 `;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 import Sun from "../../images/sun.svg";
 import HeroXl from "../../images/hero.png";
 import HeroSm from "../../images/hero-sm.png";
-import { HeroWrapper } from "./Hero.style";
+import { HeroWrapper, TagList, TagItem, TagSection } from "./Hero.style";
 
 export const Hero = () => {
   const data = ["Sport", "Healthy", "Workout", "Diet"];
@@ -10,21 +10,23 @@ export const Hero = () => {
     <>
       <HeroWrapper>
         <>
-          <img src={Sun} alt="Logo icon" />
-          <h1>Get Body in shape, Stay healthy</h1>
+          <h1>
+            <img src={Sun} alt="Logo icon" />
+            Get Body in shape, Stay healthy
+          </h1>
           <p>
             Transform your physique and embrace a healthier lifestyle with our
             comprehensive fitness and nutrition support.
           </p>
         </>
-        <>
-          <img src={HeroSm} alt="Logo icon" />
-          <ul>
+        <TagSection>
+          <img src={HeroSm} alt="Logo icon" width="168px" height="100px" />
+          <TagList>
             {data.map((i, idx) => (
-              <li key={idx}>{i}</li>
+              <TagItem key={idx}>#{i}</TagItem>
             ))}
-          </ul>
-        </>
+          </TagList>
+        </TagSection>
       </HeroWrapper>
       <img src={HeroXl} alt="Logo icon" />
     </>
